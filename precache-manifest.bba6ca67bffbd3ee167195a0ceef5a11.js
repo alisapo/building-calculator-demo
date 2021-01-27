dc68804baf41328f31bf69d06d6fc3a5fbafd18f
Alisa <mme.figaro@ya.ru>
2021-01-27 21:33:03 +0300

    Updates

---
+++ precache-manifest.bba6ca67bffbd3ee167195a0ceef5a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f479b4ba0bcccc8be85141c4f2cb50",
+    "url": "/building-calculator-demo/index.html"
+  },
+  {
+    "revision": "4691237b1a19ab755fdb",
+    "url": "/building-calculator-demo/static/css/main.b8575265.chunk.css"
+  },
+  {
+    "revision": "d0415773d89e7c052386",
+    "url": "/building-calculator-demo/static/js/2.b3962550.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/building-calculator-demo/static/js/2.b3962550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4691237b1a19ab755fdb",
+    "url": "/building-calculator-demo/static/js/main.fd0e28da.chunk.js"
+  },
+  {
+    "revision": "c9e8489121baf394f919",
+    "url": "/building-calculator-demo/static/js/runtime-main.222dd29c.js"
+  }
+]);
